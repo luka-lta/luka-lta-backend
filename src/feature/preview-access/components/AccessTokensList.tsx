c1ed1b4a4e5c6ac976ce1e2c255cd8c4f86fd5b7
luka-lta <luka@luka-lta.dev>
2025-03-27 23:58:58 +0100

    #7 Added access token create dialog

--- src/feature/preview-access/components/AccessTokensList.tsx
+++ src/feature/preview-access/components/AccessTokensList.tsx
@@ -12,6 +12,8 @@ import {Button} from "@/components/ui/button.tsx";
 import {EllipsisVertical, Pencil, Trash} from "lucide-react";
 import {Badge} from "@/components/ui/badge.tsx";
 import {cn} from "@/lib/utils.ts";
+import {useState} from "react";
+import CreateAccessTokenDialog from "@/feature/preview-access/components/CreateAccessTokenDialog.tsx";
 
 interface AccessTokensListProps {
     accessTokens: AccessTokenTypeSchema[];
@@ -22,58 +24,65 @@ interface AccessTokensListProps {
 
 function AccessTokensList({accessTokens, maxPages, loading, setFilterData}: AccessTokensListProps) {
     const queryClient = useQueryClient();
+    const [newToken, setNewToken] = useState(false);
 
     return (
-        <div className='px-6'>
-            <DataTable
-                data={accessTokens}
-                header={[
-                    {label: 'Token', sortName: 'token'},
-                    {label: 'Max Uses'},
-                    {label: 'Usages'},
-                    {label: 'Status', sortName: 'status'},
-                    {label: ''},
-                ]}
-                maxPages={maxPages}
-                renderRow={(accessToken) => {
-                    return (
-                        <TableRow key={accessToken.token}>
-                            <TableCell>{accessToken.token}</TableCell>
-                            <TableCell>{accessToken.maxUse}</TableCell>
-                            <TableCell>{accessToken.used}</TableCell>
-                            <TableCell>
-                                <Badge variant={"default"} className={cn("ml-2 bg-red-500 text-white", accessToken.isActive && "bg-green-500 text-black")}>
-                                    {accessToken.isActive ? "Active" : "Inactive"}
-                                </Badge>
-                            </TableCell>
-                            <TableCell>
-                                <DropdownMenu>
-                                    <DropdownMenuTrigger asChild>
-                                        <Button variant={'ghost'}><EllipsisVertical/></Button>
-                                    </DropdownMenuTrigger>
-                                    <DropdownMenuContent>
-                                        <DropdownMenuItem onClick={(event) => {
-                                            event.stopPropagation();
-                                        }}>
-                                            <Pencil/>
-                                            Edit
-                                        </DropdownMenuItem>
-                                        <DropdownMenuItem>
-                                            <Trash/>
-                                            Delete
-                                        </DropdownMenuItem>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
-                            </TableCell>
-                        </TableRow>
-                    );
-                }}
-                onFilterChange={setFilterData}
-                onCreateNew={() => alert('New')}
-                loading={loading}
-                onRefetchData={() => queryClient.invalidateQueries({queryKey: ['access', 'tokens', 'list']})}
-            />
-        </div>
+        <>
+            {newToken && (
+                <CreateAccessTokenDialog onClose={() => setNewToken(false)}/>
+            )}
+            <div className='px-6'>
+                <DataTable
+                    data={accessTokens}
+                    header={[
+                        {label: 'Token', sortName: 'token'},
+                        {label: 'Max Uses'},
+                        {label: 'Usages'},
+                        {label: 'Status', sortName: 'status'},
+                        {label: ''},
+                    ]}
+                    maxPages={maxPages}
+                    renderRow={(accessToken) => {
+                        return (
+                            <TableRow key={accessToken.token}>
+                                <TableCell>{accessToken.token}</TableCell>
+                                <TableCell>{accessToken.maxUse}</TableCell>
+                                <TableCell>{accessToken.used}</TableCell>
+                                <TableCell>
+                                    <Badge variant={"default"}
+                                           className={cn("ml-2 bg-red-500 text-white", accessToken.isActive && "bg-green-500 text-black")}>
+                                        {accessToken.isActive ? "Active" : "Inactive"}
+                                    </Badge>
+                                </TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant={'ghost'}><EllipsisVertical/></Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent>
+                                            <DropdownMenuItem onClick={(event) => {
+                                                event.stopPropagation();
+                                            }}>
+                                                <Pencil/>
+                                                Edit
+                                            </DropdownMenuItem>
+                                            <DropdownMenuItem>
+                                                <Trash/>
+                                                Delete
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+                            </TableRow>
+                        );
+                    }}
+                    onFilterChange={setFilterData}
+                    onCreateNew={() => setNewToken(true)}
+                    loading={loading}
+                    onRefetchData={() => queryClient.invalidateQueries({queryKey: ['access', 'tokens', 'list']})}
+                />
+            </div>
+        </>
     );
 }
 
---
+++ src/feature/preview-access/components/CreateAccessTokenDialog.tsx
@@ -0,0 +1,124 @@
+import {z} from "zod";
+import React from "react";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {SubmitHandler, useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {FetchWrapper} from "@/lib/fetchWrapper.ts";
+import {toast} from "sonner";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle
+} from "@/components/ui/dialog.tsx";
+import {TextInput} from "@/components/form/TextInput.tsx";
+import {Label} from "@/components/ui/label.tsx";
+import {Switch} from "@/components/ui/switch.tsx";
+import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert.tsx";
+import {Button} from "@/components/ui/button.tsx";
+
+const accessTokenCreateSchema = z.object({
+    maxUse: z.string().optional().default('1'),
+    isActive: z.boolean().optional().default(true),
+});
+
+type CreateAccessTokenDialogProps = {
+    onClose: () => void,
+}
+
+interface createAccessTokenData {
+    maxUse?: number,
+    isActive?: boolean,
+}
+
+export const CreateAccessTokenDialog: React.FC<CreateAccessTokenDialogProps> = ({onClose}) => {
+    const queryClient = useQueryClient();
+
+    const form = useForm<createAccessTokenData>({
+        resolver: zodResolver(accessTokenCreateSchema),
+    });
+
+    const createPreviewToken = useMutation({
+        mutationFn: async ({maxUse, isActive}: createAccessTokenData) => {
+            const fetchWrapper = new FetchWrapper(FetchWrapper.baseUrl);
+            await fetchWrapper.post('/previewToken/', {
+                maxUse: Number(maxUse),
+                isActive,
+            })
+        },
+        onSuccess: () => {
+            onClose();
+            toast.success('Preview token created successfully!');
+        },
+        onError: (error) => {
+            toast.error('Failed to create link');
+            console.error(error);
+        },
+        onSettled: () => {
+            setTimeout(() => {
+                queryClient.invalidateQueries({
+                    queryKey: ['access', 'tokens', 'list'],
+                });
+            }, 500)
+        }
+    });
+
+    const onSubmit: SubmitHandler<createAccessTokenData> = (data) => createPreviewToken.mutate(data);
+
+    return (
+        <Dialog open={true} onOpenChange={(open) => {
+            if (!open) {
+                onClose();
+            }
+        }}>
+            <DialogContent>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <DialogHeader>
+                        <DialogTitle>Create Preview-Token</DialogTitle>
+                        <DialogDescription>Create a new Preview-Token</DialogDescription>
+                    </DialogHeader>
+
+                    <div className='grid gap-6 py-6'>
+                        <TextInput
+                            name={'maxUse'}
+                            id={'preview-token-max-uses-create-form'}
+                            label={'Max Uses'}
+                            form={form}
+                            placeholder='Default: 1'
+                            type={'number'}
+                        />
+
+                        <div className="flex flex-col items-start gap-2">
+                            <Label htmlFor="active">Active</Label>
+                            <Switch
+                                id="isActive"
+                                {...form.register('isActive')}
+                                onCheckedChange={(value) => form.setValue('isActive', value)}
+                                defaultChecked={form.getValues('isActive')}
+                            />
+                        </div>
+
+                        {createPreviewToken.error && (
+                            <Alert variant='destructive'>
+                                <AlertTitle>Error, failed to create preview-token!</AlertTitle>
+                                <AlertDescription>{createPreviewToken.error.message}</AlertDescription>
+                            </Alert>
+                        )}
+                    </div>
+
+                    <DialogFooter>
+                        {createPreviewToken.isPending ? (
+                            <Button className="w-[100%]" disabled>Creating token...</Button>
+                        ) : (
+                            <Button className="w-[100%]" type='submit'>Create token</Button>
+                        )}
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+export default CreateAccessTokenDialog;
